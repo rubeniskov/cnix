40a3e22859f65c05d6de99f2926198715e263652
rubeniskov <me@rubeniskov.com>
2021-04-01 11:40:42 +0200

    feat: add conv FloatToString to vector package

---
+++ vector/conv.go
@@ -0,0 +1,22 @@
+package vector
+
+import (
+	"strconv"
+	"strings"
+)
+
+func FloatToString(f float64, p int) string {
+	x := strconv.FormatFloat(f, 'f', p, 64)
+
+	// Hacky way to remove silly zeroes
+	if strings.ContainsRune(x, '.') {
+		for x[len(x)-1] == '0' {
+			x = x[:len(x)-1]
+		}
+		if x[len(x)-1] == '.' {
+			x = x[:len(x)-1]
+		}
+	}
+
+	return x
+}
